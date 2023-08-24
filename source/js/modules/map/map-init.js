9f25098f2c1bcd912283497ae8a912fa851f70b2
Roman Tsarew <romats64@gmail.com>
2023-08-24 20:20:18 +0200

    done

--- source/js/modules/map/map-init.js
+++ source/js/modules/map/map-init.js
@@ -1,21 +1,24 @@
 /* eslint-disable no-undef */
 function mapInit() {
-  const map = L.map('map', {scrollWheelZoom: false}).setView(
-      [55.028343, 82.927639],
-      15
-  );
+  const mapElement = document.getElementById('map');
+  if (mapElement) {
+    const map = L.map('map', {scrollWheelZoom: false}).setView(
+        [55.028343, 82.927639],
+        15
+    );
 
-  L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
-    maxZoom: 20,
-    subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
-  }).addTo(map);
+    L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
+      maxZoom: 20,
+      subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+    }).addTo(map);
 
-  const basicIcon = L.icon({
-    iconUrl: 'img/svg/point-map.svg',
-    iconSize: [51, 70],
-  });
+    const basicIcon = L.icon({
+      iconUrl: 'img/svg/point-map.svg',
+      iconSize: [51, 70],
+    });
 
-  L.marker([55.028543, 82.928145], {icon: basicIcon}).addTo(map);
+    L.marker([55.028543, 82.928145], {icon: basicIcon}).addTo(map);
+  }
 }
 
 export {mapInit};
